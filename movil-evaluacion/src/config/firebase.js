a243ff5c3810570caeaa7bb769bd9eee768f2e56
EdwinAlfaro123 <20210300@ricaldone.edu.sv>
2026-09-16 07:41:16 -0600

    Feat: se implemento la estructura de carpetas y la instalación de dependencias

---
+++ movil-evaluacion/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMuT0yxZuyYqkz1SVbtuujjXuadbWJpEg",
+  authDomain: "evaluacion-movil-996bb.firebaseapp.com",
+  projectId: "evaluacion-movil-996bb",
+  storageBucket: "evaluacion-movil-996bb.firebasestorage.app",
+  messagingSenderId: "594816203351",
+  appId: "1:594816203351:web:b7ac20e2490739e18a8112"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
