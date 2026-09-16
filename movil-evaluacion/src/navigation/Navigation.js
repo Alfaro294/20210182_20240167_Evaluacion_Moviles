0d18717458c81e8cea5c5d3db95bd35444f298af
EdwinAlfaro123 <20210300@ricaldone.edu.sv>
2026-09-16 11:19:14 -0600

    Fix: se arreglo logica de usuarios

--- movil-evaluacion/src/navigation/Navigation.js
+++ movil-evaluacion/src/navigation/Navigation.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Users from '../screens/Users';
 import Login from '../screens/Login.js';
 import Home from '../screens/Home.js'
+import Add from '../screens/AddUser.js'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,8 @@ const Navigation = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="User" component={Users} options={{title:'User'}} />
-                {/*<Stack.Screen name="Add" component={Add} 
-                options={{presentation:'modal', title:'Nuevo Usuario'}}/>*/}
+                <Stack.Screen name="Add" component={Add} 
+                options={{presentation:'modal', title:'Nuevo Usuario'}}/>
                 <Stack.Screen name="Login" component={Login} options={{title:'Login'}} />
                 <Stack.Screen name="Home" component={Home} 
                 options={{presentation:'modal', title:'Home'}}/>
