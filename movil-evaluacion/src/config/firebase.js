bf65e08606f8863aa09795d0136adcfd51d66197
EdwinAlfaro123 <20210300@ricaldone.edu.sv>
2026-09-16 08:01:08 -0600

    Fix: Archivo firebase.js

--- movil-evaluacion/src/config/firebase.js
+++ movil-evaluacion/src/config/firebase.js
@@ -1,17 +1,33 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, test} from '@env';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMuT0yxZuyYqkz1SVbtuujjXuadbWJpEg",
-  authDomain: "evaluacion-movil-996bb.firebaseapp.com",
-  projectId: "evaluacion-movil-996bb",
-  storageBucket: "evaluacion-movil-996bb.firebasestorage.app",
-  messagingSenderId: "594816203351",
-  appId: "1:594816203351:web:b7ac20e2490739e18a8112"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID    
 };
+
+console.log("Valor de test: ", test);
+console.log("Valor de configuracion", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+if (app) {
+  console.log('Firebase initialized successfully');
+} else {
+  console.log('Firebase initialization failed');
+}
+
+const database = getFirestore(app);
+if (database) {
+  console.log('Firestore initialized correctly');
+} else {
+  console.log('Firestore initialization failed');
+}
+
+export { database };
